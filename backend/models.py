9f3d3c0c192e2ca3eb08e3e65c7ecd4b914a8aea
Joshua Samil Espinosa Fuentes <joshua-samil@hotmail.com>
2025-08-19 13:28:31 -0600

    Commit message: This is the first commit from vsc.

--- backend/models.py
+++ backend/models.py
@@ -17,7 +17,7 @@ from sqlalchemy.orm import declarative_base, relationship, sessionmaker
 
 Base = declarative_base()
 
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///backend/monitor.db")
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./monitor.db")
 engine = create_engine(
     DATABASE_URL,
     connect_args={"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {},
